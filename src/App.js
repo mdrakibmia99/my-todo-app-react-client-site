3fc8b12f2a6fd4cc1c284a9106ec1c636d2053d3
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-06-30 23:19:54 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Routes ,Route} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-        My todo apps
+       <Routes>
+        <Route path="/" element={<Header></Header>}></Route>
+       </Routes>
     </div>
   );
 }
